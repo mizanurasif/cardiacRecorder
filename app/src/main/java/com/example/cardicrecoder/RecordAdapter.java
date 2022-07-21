741d298b13a6e37a8061cf229fedf157c1fceea3
mizanurasif <mizanurasif02@gmail.com>
2022-07-21 21:02:44 +0600

    upd

--- app/src/main/java/com/example/cardicrecoder/RecordAdapter.java
+++ app/src/main/java/com/example/cardicrecoder/RecordAdapter.java
@@ -2,6 +2,7 @@ package com.example.cardicrecoder;
 
 import android.content.Context;
 import android.text.Layout;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,26 +17,47 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.recordview
     ArrayList<Recorditem> recorditems;
     //context for database
     Context context;
+    private OnItemClickListener onItemClickListener;
+    public interface OnItemClickListener{
+        void onClick(int position);
+
+
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
     public RecordAdapter(Context context, ArrayList<Recorditem> recorditems) {
         this.recorditems = recorditems;
         this.context=context;
     }
 
-    public static class recordviewHolder extends RecyclerView.ViewHolder{
+    public static class recordviewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
         TextView heart_beat;
         TextView status;
         TextView comment;
         TextView date;
         TextView time;
-        public recordviewHolder(@NonNull View itemView) {
+        public recordviewHolder(@NonNull View itemView,OnItemClickListener onItemClickListener) {
             super(itemView);
             heart_beat=itemView.findViewById(R.id.hbm_s);
             status=itemView.findViewById(R.id.status);
             comment=itemView.findViewById(R.id.comment_s);
             date=itemView.findViewById(R.id.date);
             time=itemView.findViewById(R.id.time);
+            itemView.setOnClickListener(v->onItemClickListener.onClick(getAdapterPosition()));
+            itemView.setOnCreateContextMenuListener(this);
 
 
+
+
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+            contextMenu.add(getAdapterPosition(),0,0,"Edit");
+            contextMenu.add(getAdapterPosition(),1,0,"Delate");
         }
     }
 
@@ -43,7 +65,7 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.recordview
     @Override
     public recordviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemview= LayoutInflater.from(parent.getContext()).inflate(R.layout.record_item,parent,false);
-        return new recordviewHolder(itemview);
+        return new recordviewHolder(itemview,onItemClickListener);
     }
 
     @Override
